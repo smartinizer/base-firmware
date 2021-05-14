8491d468cdbb19ed46cea1b334a4823c7643a4fa
David <davod2211@gmail.com>
2021-05-14 15:35:55 +0200

    Refactored the core into a lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,13 @@
-#include <iostream>
-#include <SPIFFS.h>
-#include <config.hh>
-#include <connection.hh>
-#include <ota.hh>
-#include <webserver.hh>
+#include <Smartinizer.h>
 
-
-Connection* connectionHandler;
-WebServer* webserver;
-
+Smartinizer* sm;
 
 void setup() {
-    Serial.begin(115200);
-    if(!SPIFFS.begin()){
-        Serial.println("An Error has occurred while mounting SPIFFS");
-    }
-
-    connectionHandler = new Connection();
-  
-    using namespace std;
-    String ssid, pw;
-    tie(ssid, pw) = config::getWifiCredentialsfromwpaconf();
-    if(ssid!=""){
-      connectionHandler->wifi_sta_setup(ssid.c_str(), pw.c_str());
-    }else{
-      Serial.println("Cant find config");
-      connectionHandler->wifi_ap_setup();
-    }
-    update::update_if_sheduled();
-    config::downloadFirmwareList("https://smartinizer.devzero.cloud/firmwares.json");
-    webserver = new WebServer();
+    sm = new Smartinizer();
+    sm->setup();
 }
 
 
 void loop() {
-  
+  sm->loop();
 }
