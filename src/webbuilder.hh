88871afada6c51f94ab958934a443267a4587d12
Robert Franzke <35638696+r-franzke@users.noreply.github.com>
2021-05-14 09:52:20 +0200

    Implemented Configuration for new Firmware (#13)
    In order to install a new firmware I added the workflow to configure the firmware. Right now nothing will be installed. The Update process will be implemented in the next step.

--- src/webbuilder.hh
+++ src/webbuilder.hh
@@ -1,18 +1,31 @@
 #include <string>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
+#include <HTTPClient.h>
 
 
 namespace webbuilder{
+
+    JsonArray getFirmwares(){
+        StaticJsonDocument<2014> doc;
+        deserializeJson(doc, config::getFirmwareList());
+        JsonObject obj = doc.as<JsonObject>();
+        return obj["firmwares"];
+    }
+
+
     // Takes the config.html from the SPIFFS and injects the available firmwares as RadioButtons
     String getConfigPage(){
         File file = SPIFFS.open("/config.html", "r");
-        StaticJsonDocument<1024> doc;
+        Serial.println("Opened config.html");
+        StaticJsonDocument<2014> doc;
         deserializeJson(doc, config::getFirmwareList());
         JsonObject obj = doc.as<JsonObject>();
         JsonArray firmwares = obj["firmwares"];
+        Serial.println("Loaded Firmwares");
         String htmlOptions = "";
         for (int i = 0; i < firmwares.size(); i++) {
+            Serial.println("Loop Runde " + i);
             String htmlOption = "<input type=\"radio\" id=\"$ID\" name=\"firmware\" value=\"$NAME\" $CHECKED><label for=\"$NAME\"><b>$NAME</b> - $DESCRIPTION</label>";
             htmlOption.replace("$ID", String(i));
             String name = firmwares[i]["name"];
@@ -26,11 +39,39 @@ namespace webbuilder{
                 htmlOption.replace("$CHECKED","");
                 htmlOptions = htmlOptions + "<br>" + htmlOption;
             }
-            
         }
         String htmlPage = file.readString();
         htmlPage.replace("$OPTIONS",htmlOptions);
+        Serial.println("End of getConfigPage");
         return htmlPage;
     }
+
+    String getFirmwareConfigPage(String firmwareName){
+        HTTPClient http;
+        File file = SPIFFS.open("/config-firmware.html", "r");
+        StaticJsonDocument<2014> doc;
+        deserializeJson(doc, config::getFirmwareList());
+        JsonObject obj = doc.as<JsonObject>();
+        JsonArray firmwares = obj["firmwares"];
+        // Looking for Config
+        String configUrl = "";
+        for(int i = 0; i < firmwares.size(); i++){
+            Serial.println("Compare: " + firmwares[i]["name"].as<String>() + " = " + firmwareName);
+            if(firmwares[i]["name"] == firmwareName){
+                configUrl = firmwares[i]["url"].as<String>();
+                Serial.println("Found ConfigUrl for " + firmwareName + " at " + configUrl);
+                break;
+            }
+        }
+        
+        Serial.println("Firmware Config URL: " + configUrl);
+        String configJson = config::getFirmwareConfig(configUrl);
+        String pageHTML = file.readString();
+        pageHTML.replace("$CONFIG", configJson);
+        file.close();
+        return pageHTML;
+    }
+
+
 
 }
